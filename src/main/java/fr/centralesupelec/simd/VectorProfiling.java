98219d9d0bad8590c84dedf1ffe61e2a5f132063
delthas <delthas@dille.cc>
2019-01-09 13:50:59 +0100

    benchmark: add filterSum

--- src/main/java/fr/centralesupelec/simd/VectorProfiling.java
+++ src/main/java/fr/centralesupelec/simd/VectorProfiling.java
@@ -1,6 +1,7 @@
 package fr.centralesupelec.simd;
 
 import jdk.incubator.vector.*;
+import jdk.incubator.vector.Vector.*;
 import org.openjdk.jmh.annotations.*;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -9,28 +10,28 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class VectorProfiling {
 
-
     @State(Scope.Thread)
     public static class VectorState {
 
         @Param({"512", "1024", "2048", "4096", "8192", "16384", "32768", "65536", "131072", "262144", "524288", "1048576", "2097152", "4194304", "8388608", "16777216", "33554432", "67108864"})
-        public int array_size;
+        public int ARRAY_LENGTH;
 
         private static final IntVector.IntSpecies<?> sInt = IntVector.preferredSpecies();
         private static final int vecLength = sInt.length();
         public static final int bitSize = sInt.bitSize();
-        int[] data = new int[array_size];
-        int[] datb = new int[array_size];
-        int[] datc = new int[array_size];
+        int[] a = new int[ARRAY_LENGTH];
+        int[] b = new int[ARRAY_LENGTH];
+        int[] c = new int[ARRAY_LENGTH];
+        int filter;
 
         @Setup(Level.Trial)
         public void doSetup() {
-            System.out.println(sInt.bitSize());
             Random rnd = new Random();
-            for (int i = 0; i < data.length; i++) {
-                data[i] = rnd.nextInt();
-                datb[i] = rnd.nextInt();
+            for (int i = 0; i < a.length; i++) {
+                a[i] = rnd.nextInt();
+                b[i] = rnd.nextInt();
             }
+            filter = Integer.MAX_VALUE / 2 + rnd.nextInt(1000000) - 500000;
         }
     }
 
@@ -39,8 +40,8 @@ public class VectorProfiling {
     @Measurement(iterations=3, time=2)
     public long sumSIMD(VectorState state) {
         long sum = 0;
-        for(int i = 0; i < state.data.length; i += VectorState.vecLength) {
-            sum += VectorState.sInt.fromArray(state.data, i).addAll();
+        for(int i = 0; i < state.a.length; i += VectorState.vecLength) {
+            sum += VectorState.sInt.fromArray(state.a, i).addAll();
         }
         return sum;
     }
@@ -50,8 +51,8 @@ public class VectorProfiling {
     @Measurement(iterations=3, time=2)
     public long sumRegular(VectorState state) {
         long sum = 0;
-        for(int i = 0; i < state.data.length; ++i) {
-            sum += state.data[i];
+        for(int i = 0; i < state.a.length; ++i) {
+            sum += state.a[i];
         }
         return sum;
     }
@@ -62,8 +63,8 @@ public class VectorProfiling {
     @Measurement(iterations = 3, time = 2)
     public long sumRegularNoSuperWord(VectorState state) {
         long sum = 0;
-        for (int i = 0; i < state.data.length; ++i) {
-            sum += state.data[i];
+        for (int i = 0; i < state.a.length; ++i) {
+            sum += state.a[i];
         }
         return sum;
     }
@@ -72,23 +73,23 @@ public class VectorProfiling {
     @Warmup(iterations=2, time=2)
     @Measurement(iterations=3, time=2)
     public int[] mulSIMD(VectorState state) {
-        for(int i = 0; i < state.data.length; i += VectorState.vecLength) {
-            IntVector va = VectorState.sInt.fromArray(state.data, i);
-            IntVector vb = VectorState.sInt.fromArray(state.datb, i);
+        for(int i = 0; i < state.a.length; i += VectorState.vecLength) {
+            IntVector va = VectorState.sInt.fromArray(state.a, i);
+            IntVector vb = VectorState.sInt.fromArray(state.b, i);
             IntVector vc = va.mul(vb);
-            vc.intoArray(state.datc, i);
+            vc.intoArray(state.c, i);
         }
-        return state.datc;
+        return state.c;
     }
 
     @Benchmark
     @Warmup(iterations=2, time=2)
     @Measurement(iterations=3, time=2)
     public int[] mulRegular(VectorState state) {
-        for(int i = 0; i < state.data.length; ++i) {
-            state.datc[i] = state.data[i] * state.datb[i];
+        for(int i = 0; i < state.a.length; ++i) {
+            state.c[i] = state.a[i] * state.b[i];
         }
-        return state.datc;
+        return state.c;
     }
 
     @Benchmark
@@ -96,33 +97,33 @@ public class VectorProfiling {
     @Warmup(iterations = 2, time = 2)
     @Measurement(iterations = 3, time = 2)
     public int[] mulRegularNoSuperWord(VectorState state) {
-        for (int i = 0; i < state.data.length; ++i) {
-            state.datc[i] = state.data[i] * state.datb[i];
+        for (int i = 0; i < state.a.length; ++i) {
+            state.c[i] = state.a[i] * state.b[i];
         }
-        return state.datc;
+        return state.c;
     }
 
     @Benchmark
     @Warmup(iterations = 2, time = 2)
     @Measurement(iterations = 3, time = 2)
     public int[] addSIMD(VectorState state) {
-        for (int i = 0; i < state.data.length; i += VectorState.vecLength) {
-            IntVector va = VectorState.sInt.fromArray(state.data, i);
-            IntVector vb = VectorState.sInt.fromArray(state.datb, i);
+        for (int i = 0; i < state.a.length; i += VectorState.vecLength) {
+            IntVector va = VectorState.sInt.fromArray(state.a, i);
+            IntVector vb = VectorState.sInt.fromArray(state.b, i);
             IntVector vc = va.add(vb);
-            vc.intoArray(state.datc, i);
+            vc.intoArray(state.c, i);
         }
-        return state.datc;
+        return state.c;
     }
 
     @Benchmark
     @Warmup(iterations = 2, time = 2)
     @Measurement(iterations = 3, time = 2)
     public int[] addRegular(VectorState state) {
-        for (int i = 0; i < state.data.length; ++i) {
-            state.datc[i] = state.data[i] + state.datb[i];
+        for (int i = 0; i < state.a.length; ++i) {
+            state.c[i] = state.a[i] + state.b[i];
         }
-        return state.datc;
+        return state.c;
     }
 
     @Benchmark
@@ -130,9 +131,49 @@ public class VectorProfiling {
     @Warmup(iterations = 2, time = 2)
     @Measurement(iterations = 3, time = 2)
     public int[] addRegularNoSuperWord(VectorState state) {
-        for (int i = 0; i < state.data.length; ++i) {
-            state.datc[i] = state.data[i] + state.datb[i];
+        for (int i = 0; i < state.a.length; ++i) {
+            state.c[i] = state.a[i] + state.b[i];
         }
-        return state.datc;
+        return state.c;
+    }
+
+    @Benchmark
+    @Warmup(iterations = 2, time = 2)
+    @Measurement(iterations = 3, time = 2)
+    public long filterSumSIMD(VectorState state) {
+        long sum = 0;
+        for (int i = 0; i < state.a.length; i += VectorState.vecLength) {
+            IntVector va = VectorState.sInt.fromArray(state.a, i);
+            Mask mask = va.lessThanEq(state.filter);
+            sum += VectorState.sInt.fromArray(state.b, i).addAll(mask);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @Warmup(iterations = 2, time = 2)
+    @Measurement(iterations = 3, time = 2)
+    public long filterSumRegular(VectorState state) {
+        long sum = 0;
+        for (int i = 0; i < state.a.length; ++i) {
+            if(state.a[i] <= state.filter) {
+                sum += state.b[i];
+            }
+        }
+        return sum;
+    }
+
+    @Benchmark
+    @Fork(jvmArgsAppend = { "--add-modules", "jdk.incubator.vector", "-XX:-UseSuperWord" })
+    @Warmup(iterations = 2, time = 2)
+    @Measurement(iterations = 3, time = 2)
+    public long filterSumRegularNoSuperWord(VectorState state) {
+        long sum = 0;
+        for (int i = 0; i < state.a.length; ++i) {
+            if(state.a[i] <= state.filter) {
+                sum += state.b[i];
+            }
+        }
+        return sum;
     }
 }
