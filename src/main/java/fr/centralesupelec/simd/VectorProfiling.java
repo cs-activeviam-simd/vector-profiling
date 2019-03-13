1b8ea779f711b68a96310a52a39083753d9b12b6
Michelle Evans Bouchet <michelle.evans-bouchet@student.ecp.fr>
2019-03-13 15:03:48 +0100

    fix: include old parameters

--- src/main/java/fr/centralesupelec/simd/VectorProfiling.java
+++ src/main/java/fr/centralesupelec/simd/VectorProfiling.java
@@ -19,7 +19,7 @@ public class VectorProfiling {
     @State(Scope.Thread)
     public static class VectorState {
 
-        @Param({"512", "1024"})
+        @Param({"512", "1024", "2048", "4096", "8192", "16384", "32768", "65536", "131072", "262144", "524288", "1048576", "2097152", "4194304", "8388608", "16777216", "33554432", "67108864"})
         public int ARRAY_LENGTH;
         private static final int ARRAY_BOUND = 12;
 
